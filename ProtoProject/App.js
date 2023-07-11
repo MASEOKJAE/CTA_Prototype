9734c192877b05a047a079e2692c4c3f4ea0681e
MASEOKJAE <maasj7514@gmail.com>
2023-07-12 01:51:53 +0900

    Using UseState & Function

--- ProtoProject/App.js
+++ ProtoProject/App.js
@@ -1,16 +1,20 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Peter');
+  const [person, setPerson] = useState({name: 'mario', day: 'Sunday'});
+
+  const clickHandler = () => {
+    setName('Jack');
+  }
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello, World!</Text>
-      </View>
-      <View style={styles.body}>
-        <Text style={styles.boldText}>Have a good day</Text>
-        <Text>Have a good day</Text>
-        <Text>Have a good day</Text>
+      <Text style={styles.boldText}>Hello {name} Parker ~</Text>
+      <Text>Hi {person.name}, have a nice {person.day} </Text>
+      <View style={styles.buttonContainer}>
+        <Button title='updat state' onPress={clickHandler} />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -35,5 +39,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'yellow',
     padding: 30,
     fontWeight: 'bold'
+  },
+  buttonContainer: {
+    marginTop: 20
   }
 });
